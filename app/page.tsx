900c0c1dc1f90c468e3ef95009a112908383c339
BadreddineZatout <hb_zatout@esi.dz>
2024-06-08 12:57:42 +0100

    feat: products list

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,62 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Image,
+  CardFooter,
+  Link,
+} from "@nextui-org/react";
 
 import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 
-export default function Home() {
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  image: string;
+};
+
+async function getData() {
+  const res = await fetch(`${siteConfig.api_url}/products?sort=desc`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-        <br />
-        <h1 className={title()}>
-          websites regardless of your design experience.
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </h2>
+    <main className="w-full flex justify-between items-start">
+      <div className="w-1/4">Filters</div>
+      <div className="w-3/4 grid grid-cols-3 gap-5">
+        {data.map((product: Product) => (
+          <Card key={product.id} className="py-4 col-span-1 h-fit">
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+              <p className="text-tiny uppercase font-bold">{product.price}$</p>
+              <small className="text-default-500">{product.category}</small>
+              <h4 className="font-bold text-large">{product.title}</h4>
+            </CardHeader>
+            <CardBody className="overflow-hidden py-2 w-[270]">
+              <Image
+                alt="Card background"
+                className="object-cover rounded-xl"
+                src={product.image}
+                width={270}
+              />
+              <CardFooter className="flex justify-end">
+                <Link className="text-lg" href={`/products/${product.id}`}>
+                  Details
+                </Link>
+              </CardFooter>
+            </CardBody>
+          </Card>
+        ))}
       </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="flat">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
-    </section>
+    </main>
   );
 }
